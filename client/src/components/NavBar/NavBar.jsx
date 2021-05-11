83afc83fb62fc5eed897a373d37d2779e46202d0
unknown <nicolapirruccio@icloud.com>
2021-05-10 20:55:34 -0700

    navbar and homepage changes

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,8 +2,11 @@ import { Fragment, useState } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 
 import {
-  ChartBarIcon,
+  TagIcon,
+  ShoppingBagIcon,
   MenuIcon,
+  SearchIcon,
+  QuestionMarkCircleIcon,
   ShieldCheckIcon,
   SupportIcon,
   XIcon,
@@ -14,21 +17,40 @@ import SearchModal from '../SearchModal/SearchModal';
 
 const solutions = [
   {
-    name: 'Analytics',
-    description: 'Get a better understanding of where your traffic is coming from.',
-    href: '/',
-    icon: ChartBarIcon,
+    name: 'Popular Products',
+    description: 'View popular cosmetic products.',
+    href: '/popular',
+    icon: TagIcon,
+  },
+  {
+    name: 'Brands',
+    description: 'All the brands we collect products from.',
+    href: '/brands',
+    icon: ShoppingBagIcon,
+  },
+  {
+    name: 'About',
+    description: 'All about Glitzher.',
+    href: '/about',
+    icon: QuestionMarkCircleIcon,
   },
 ]
+
 const resources = [
   {
-    name: 'Help Center',
-    description: 'Get all of your questions answered in our forums or contact support.',
-    href: '/',
+    name: 'Terms of Service',
+    description: 'Learn the terms of using our service.',
+    href: '/tos',
     icon: SupportIcon,
   },
-  { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '/', icon: ShieldCheckIcon },
+  {
+    name: 'Privacy Policy',
+    description: 'Understand how we take your privacy seriously.',
+    href: '/polices/privacy',
+    icon: ShieldCheckIcon,
+  },
 ]
+
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -48,6 +70,8 @@ export default function Navbar() {
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6">
             <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+              
+              {/* Logo */}
               <div className="flex justify-start">
                 <a href="/">
                   <span className="sr-only">Workflow</span>
@@ -58,32 +82,51 @@ export default function Navbar() {
                   />
                 </a>
               </div>
+
+              {/* Mobile SearchBar */}
+              <div onClick={openModal} className="flex flex-grow items-center border p-3 mx-3 border-gray-300 rounded-md md:hidden cursor-pointer">
+                <div className="flex">
+                  <div className="p-1">
+                    <SearchIcon className="text-gray-400 h-5 w-5" aria-hidden="true" />
+                  </div>
+                  <div className="flex ml-2 my-auto text-gray-400">Search</div>
+                </div>
+              </div>
+
+              {/* Mobile Icon */}
               <div className="-mr-2 -my-2 md:hidden">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-400">
                   <span className="sr-only">Open menu</span>
                   <MenuIcon className="h-6 w-6" aria-hidden="true" />
                 </Popover.Button>
               </div>
+
+              {/* Desktop Menu Items */}
               <Popover.Group as="nav" className="hidden md:flex space-x-10">
-                <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                  Search for a product
+                <a href="/popular" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                  Popular Products
                 </a>
-                <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                <a href="/brands" className="text-base font-medium text-gray-500 hover:text-gray-900">
                   Brands
                 </a>
+                <a href="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                  About
+                </a>
               </Popover.Group>
-              <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                <div className="flex" onClick={openModal}>
-                  <button className="bg-red-500 hover:bg-red-600 rounded-md text-white p-3 focus:outline-none">
-                      <p className="font-semibold text-xl">
-                          <svg xmlns="http://www.w3.org/2000/svg" className="text-white h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                              <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-                          </svg>
-                      </p>
-                  </button>
+
+              {/* Seperator */}
+              <div className="hidden md:flex justify-between lg:w-1/5"></div>
+
+              {/* Desktop SearchBar */}
+              <div onClick={openModal} className="hidden md:flex items-center border p-3 border-gray-300 rounded-md cursor-pointer md:flex-1 lg:w-0">
+                <div className="flex">
+                  <div className="p-1">
+                    <SearchIcon className="text-gray-400 h-5 w-5" aria-hidden="true" />
+                  </div>
+                  <div className="flex ml-2 my-auto text-gray-400">Search</div>
                 </div>
-                <SearchModal isOpen={isOpen} closeModal={closeModal} openModal={isOpen} />
               </div>
+              <SearchModal isOpen={isOpen} closeModal={closeModal} openModal={isOpen} />
             </div>
           </div>
           
@@ -137,36 +180,23 @@ export default function Navbar() {
                 </div>
                 <div className="py-6 px-5 space-y-6">
                   <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                    <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                      Pricing
-                    </a>
-
-                    <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                      Docs
-                    </a>
                     {resources.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className="text-base font-medium text-gray-900 hover:text-gray-700"
+                        className="flex text-base font-medium text-gray-900 hover:text-gray-700"
                       >
-                        {item.name}
+                        <item.icon className="flex-shrink-0 h-6 w-6 text-red-500" aria-hidden="true"/>
+                        <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
                       </a>
                     ))}
                   </div>
-                  <div>
-                    <a
-                      href="/"
-                      className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-500 hover:bg-red-600"
-                    >
-                      Sign up
-                    </a>
-                    <p className="mt-6 text-center text-base font-medium text-gray-500">
-                      Existing customer?{' '}
-                      <a href="/" className="text-red-600 hover:text-red-500">
-                        Sign in
-                      </a>
-                    </p>
+                  <div
+                    onClick={openModal}
+                    className="w-full flex items-center justify-center p-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-500 hover:bg-red-600"
+                  >
+                    <SearchIcon className="flex-shrink-0 h-6 w-6 text-white" aria-hidden="true" />
+                    <span className="ml-3">Search</span>
                   </div>
                 </div>
               </div>
