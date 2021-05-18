42af9019ac29737873a87899db80f10c58e7aace
unknown <nicolapirruccio@icloud.com>
2021-05-17 22:33:38 -0700

    added new cookie banner

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,6 @@ export { default as SkeletonLoader } from './SkeletonLoaders/SkeletonLoader';
 export { default as ComparisonSkeleton } from './SkeletonLoaders/ComparisonSkeleton';
 export { default as FooterBar } from './FooterBar/FooterBar';
 export { default as Error } from './Error/Error';
+export { default as CookieBanner } from './CookieBanner/CookieBanner';
 
 export { default as ComparisonSearch } from './ComparisonSearch/ComparisonSearch';
